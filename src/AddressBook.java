e61ce145ea5ec4ffa10b011e9935cd5392b20c30
Owner-Bhavesh <bhaveshbhatia25@gmail.com>
2024-09-16 16:07:10 -0400

    Tested the print

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -8,21 +8,28 @@ public class AddressBook {
 
     /**
      * adds to the array list
+     *
      * @param name is the object to add
      */
-    public void addBuddy(BuddyInfo name){
+    public void addBuddy(BuddyInfo name) {
         book.add(name);
     }
 
     /**
      * removes Object from array list
+     *
      * @param name is the object to remove
      */
-    public void removeBuddy(BuddyInfo name){
+    public void removeBuddy(BuddyInfo name) {
         book.remove(name);
     }
 
     public static void main(String[] args) {
         System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", 613);
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        System.out.println(buddy.getName() + " " + buddy.getAddress() + " " + buddy.getPhoneNumber());
+        addressBook.removeBuddy(buddy);
     }
 }
